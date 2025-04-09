5da505ef171265768df2229dbf97399f6819e694
Viictorclay <victorcllay.pe@gmail.com>
2025-04-08 21:25:32 -0300

    08/04/2025

---
+++ DSAFIO-050.py
@@ -0,0 +1,29 @@
+#               MENOR E MAIOR VALOR DE UMA LISTA
+
+
+maior = menor = 0
+valores = []
+for c in range(0, 5):
+    valores.append(int(input(f" Digite um valor {c}: ")))
+print(f"{"=-=" * 30}")
+
+print(f"Você digtou os valores {valores} ")
+for c, v in enumerate(valores):
+#   print(f" Você digitou {v} na posições {c}..")
+    if c == 0:
+        maior = v
+        menor = v
+    else:
+        if v > maior:
+            maior = v
+        elif v < menor:
+            menor = v
+print(f" O maior valor digitado foi {maior} nas posições ", end='')
+for i, v in enumerate(valores):
+    if v == maior:
+        print(f"{i}.. ", end='')
+
+print(f"\n O menor valor digitado foi {menor} nas posições ", end='')
+for i, v in enumerate(valores):
+    if v == menor:
+        print(f"{i}.. ", end='')
